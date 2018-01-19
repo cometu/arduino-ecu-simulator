a93fd8eca2255ae8c59c953b78a170526e15a21e
cometu <come.on.ct@gmail.com>
2018-01-19 20:20:50 +0100

    quick save

--- ecu-simulator.ino
+++ ecu-simulator.ino
@@ -3,22 +3,44 @@
 #include <SPI.h>
 #include "mcp_can.h"
 
-
-
 INT32U canId = 0x000;
 
+typedef unsigned char byte_t;
 typedef unsigned char uint8_t;
 typedef char int8_t;
 
-//2024   4   65   12   18   248   185   147
- 
+#define IS_A_FLOW_CONTROL_FRAME(X) (X[0] & 0xF0 == 3)
+#define FLOW_CONTROL_FRAME_IS_CONTINUE(X) (X[0] & 0x0F == 0)
+#define MODE9_PID2_VIN 0x02
+
+#define FRAME_TYPE_REQUEST 0x00
+#define FRAME_TYPE_SINGLE 0x00
+#define FRAME_TYPE_FIRST 0x01
+#define FRAME_TYPE_CONSECUTIVE 0x02
+#define FRAME_TYPE_FLOW_CONTROL 0x03
+
+#define ANSWER_TO_MODE9 0x40 + 0x09
+
+typedef struct state{
+    uint8_t unansweredRequestNumber = 0;
+    request_t unansweredrRequests[10];
+    request_t answerInProgress
+}state_t;
+
+typedef enum states{
+    STATE_WAIT_FLOW_CONTROL_FRAME = 0,
+    STATE_SEND_NEXT_CONSECUTIVE_FRAME,
+    STATE_ANSWER_OBD_REQUEST,
+    STATE_IDLE
+} states_t;
+
+char[18] VIN = "3FADP4FJ2BM11391";
 
 uint8_t len = 0;
 uint8_t buf[8];
 char str[20];
 
-String BuildMessage="";
-int MSGIdentifier=0;
+states_t state = STATE_IDLE;
 
 void setup()
 {
@@ -51,51 +73,70 @@ void loop()
     uint8_t MilCleared[7] =         {4, 65, 63, 34, 224, 185, 147}; 
     
     //SENSORS
-    uint8_t rpmFrame[7] =                {0x04, 0x40 & 0x01, 0x0C, rpm1, rpm2, 0, 0};
-    uint8_t speedFrame[7] =             {0x04, 0x40 & 0x01, 0x0D, speed, 0, 0, 0};
+    uint8_t rpmFrame[7] =              {0x04, 0x40 & 0x01, 0x0C, rpm1, rpm2, 0, 0};
+    uint8_t speedFrame[7] =            {0x04, 0x40 & 0x01, 0x0D, speed, 0, 0, 0};
     uint8_t loadFrame[7] =             {0x04, 0x40 & 0x01, 0x04, load, 0, 0, 0};
 
     if(CAN_MSGAVAIL == CAN.checkReceive())  
     {
-      
-      CAN.readMsgBuf(&len, buf); 
+        CAN.readMsgBuf(&len, buf); 
         canId = CAN.getCanId();
         Serial.print("<");Serial.print(canId);Serial.print(",");
 
-        for(int i = 0; i<len; i++)
-        {  
-          BuildMessage = BuildMessage + buf[i] + ",";
-        }
-        Serial.println(BuildMessage);
-
-        uint8_t len = buf[1];
-        uint8_t mode = buf[1];
-        uint8_t pid = buf[2];
-
-
-        if ( mode == 0x02)
+        if ( state == STATE_IDLE )
         {
-            switch ( pid ) 
+            uint8_t len = buf[1];
+            uint8_t mode = buf[1];
+            uint8_t pid = buf[2];
+
+            if ( mode == 0x02)
             {
-                case 0x01:
-                    CAN.sendMsgBuf(0x7E8, 0, 1, SupportedPID);
-                  break;
-                case 0x04:
-                  CAN.sendMsgBuf(0x7E8, 0, 7, loadFrame);
-                  break;
-                case 0x0C:
-                    CAN.sendMsgBuf(0x7E8, 0, 7, rpmFrame);
-                    break;
-                case 0x0D:
-                    CAN.sendMsgBuf(0x7E8, 0, 7, speedFrame);
-                    break;
-                default:
-                  // do something
+                switch ( pid ) 
+                {
+                    case 0x01:
+                        CAN.sendMsgBuf(0x7E8, 0, 1, SupportedPID);
+                      break;
+                    case 0x04:
+                      CAN.sendMsgBuf(0x7E8, 0, 7, loadFrame);
+                      break;
+                    case 0x0C:
+                        CAN.sendMsgBuf(0x7E8, 0, 7, rpmFrame);
+                        break;
+                    case 0x0D:
+                        CAN.sendMsgBuf(0x7E8, 0, 7, speedFrame);
+                        break;
+                    default:
+                      // do something
+                }
+            }
+            else if ( mode == 0x09 )
+            {
+                if ( pid == 0x02 )
+                {
+                    byte_t payload = {
+                        FRAME_TYPE_FIRST & 0x00,
+                        0x14,
+                        ANSWER_TO_MODE9,
+                        MODE9_PID2_VIN,
+                        0x01,
+                        VIN[0],
+                        VIN[1],
+                        VIN[2]
+                    }
+                    CAN.sendMsgBuf(0x7E8, 0, 8, (byte_t){0x10 & 0x00, 0x14, 0x49, 0x02, 0x01 });
+                }
             }
         }
-        else if ( mode == 0x09 )
+        else if ( state == STATE_WAIT_FLOW_CONTROL_FRAME )
         {
-            
+            if ( IS_A_FLOW_CONTROL_FRAME ( buf ) )
+            {
+                if ( FLOW_CONTROL_FRAME_IS_CONTINUE(buf) )
+                {
+                    state = STATE_SEND_NEXT_CONSECUTIVE_FRAME;
+                    send_next_consecutive_frame();
+                }
+            }
         }
     }
 }
